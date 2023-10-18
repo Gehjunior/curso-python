60e91ba0cd9dc3ccef14c370a453d09c154db191
Gehjunior <geraldoluiz93@gmail.com>
2023-10-17 21:51:36 -0300

    Exercicios Secao 4

--- Exercise/Secao_4/exercicio1.py
+++ Exercise/Secao_4/exercicio1.py
@@ -1,3 +1,12 @@
+
+'''
+#Faça um programa que leia um número inteiro e o imprima.
+'''
+# Imports de bibliotecas
+import os
+
 print('Informe um número: ')
 num = input()
 print(f'O número ditado é: {num}')
+
+os.system("pause")
