24c2d64f03605dae3ed66b3a7c2efe2c84a446b9
Gehjunior <geraldoluiz93@gmail.com>
2023-10-24 09:22:16 -0300

    Exercicios Secao 4

---
+++ Exercise/Secao_4/exercicio4.py
@@ -0,0 +1,11 @@
+'''
+# Leia um número real e imprima o quadrado desse número.
+'''
+import os
+import math
+
+num = float(input('Informe um número: \n'))
+qdnum = num ** 2
+print(f'O quadrado de: {num} \né igual a {qdnum}')
+
+os.system("pause")
